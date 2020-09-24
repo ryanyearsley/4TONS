a006eae92d98fd1b007594df37ada97d3edcfdc9
Ben Cutler <b.cutler724@gmail.com>
2020-09-23 20:26:25 -0400

    Added the base animations for the Wizard of Light

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,16 +6,21 @@ namespace PlayerManagement {
 	[RequireComponent (typeof (MovementController))]
 	public class Player : MonoBehaviour {
 		public float moveSpeed = 6f;
+		[Tooltip ("Scaling amount used to adjust the speeds on different axis.")]
+		public Vector2 velocityScaling = new Vector2 (1f, 1f);
 		[Range (0.01f, 0.9f)]
 		public float acceleration = 0.1f;
 		public float dashSpeedMultiplier = 4f;
 		[Tooltip ("In seconds.")]
 		[Range (0.01f, 1f)] public float dashDuration = 0.2f;
 
+		public SpriteRenderer sprite;
+
 		private Vector2 velocity;
 		private Vector2 velocitySmoothing;
 
 		private MovementController controller;
+		private Animator animator;
 
 		private Vector2 directionalInput;
 
@@ -23,12 +28,25 @@ namespace PlayerManagement {
 
 		private void Start () {
 			controller = GetComponent<MovementController> ();
+			animator = GetComponent<Animator> ();
 		}
 
 		private void FixedUpdate () {
 			CalculateVelocity ();
 
 			controller.Move (velocity * Time.fixedDeltaTime, directionalInput);
+
+			if (animator != null) {
+				if (directionalInput != Vector2.zero && controller.collisions.moveAmountOld != Vector2.zero) {
+					animator.SetBool ("isWalking", true);
+				} else {
+					animator.SetBool ("isWalking", false);
+				}
+
+				if (sprite != null) {
+					sprite.flipX = (Mathf.Sign (directionalInput.x) == -1);
+				}
+			}
 		}
 
 		public void SetDirectionalInput (Vector2 input) {
@@ -39,6 +57,9 @@ namespace PlayerManagement {
 			if (!isDashing) {
 				isDashing = true;
 				StartCoroutine (ResetIsDashing (dashDuration));
+
+				if (animator != null)
+					animator.SetTrigger ("rollDodge");
 			}
 		}
 
@@ -47,7 +68,11 @@ namespace PlayerManagement {
 		}
 
 		private void CalculateVelocity () {
-			Vector2 targetVelocity = directionalInput.normalized * moveSpeed;
+			Vector2 normalizedInput = directionalInput;
+			if (normalizedInput.sqrMagnitude > 1f)
+				normalizedInput = normalizedInput.normalized;
+
+			Vector2 targetVelocity = normalizedInput * (moveSpeed * velocityScaling);
 
 			if (isDashing) {
 				targetVelocity *= dashSpeedMultiplier;
