3b92fc1ebf8ad3a34c48d3996653057103c6e1aa
skiddmcmarx <41240083+skiddmcmarx@users.noreply.github.com>
2020-10-04 22:52:16 -0400

    Added Isometric Coord Utility + tests

---
+++ Assets/Scripts/Test/IsometricCoordTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IsometricCoordTest : MonoBehaviour
+{
+    public GameObject trackingObject;
+    public Text coordinateText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        coordinateText.text = IsometricCoordinateUtils.TranslateSceneToIso(trackingObject.transform.position).ToString();
+    }
+}
