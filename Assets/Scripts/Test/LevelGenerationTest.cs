0073c989e5c09ba5352b26a9b3cb1921f01aebbf
skiddmcmarx <41240083+skiddmcmarx@users.noreply.github.com>
2020-10-05 12:49:06 -0400

    Added LevelFactory + test scene/prefabs

---
+++ Assets/Scripts/Test/LevelGenerationTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerationTest : MonoBehaviour
+{
+    [SerializeField]
+    private TextAsset levelDataCsv;
+
+    [SerializeField]
+    private GameObject[] blockSet;
+
+    [SerializeField]
+    private int[,] levelData;
+
+    void Start()
+    {
+        levelData = LevelFactory.DeserializeLevelFile(levelDataCsv);
+        LevelFactory.BuildLevel(levelData, blockSet);   
+    }
+}
