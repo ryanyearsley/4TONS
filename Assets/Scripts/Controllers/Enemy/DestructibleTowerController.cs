29c52c7af25050a72d17be4d20601ccc25c35edd
skiddmcmarx <41240083+skiddmcmarx@users.noreply.github.com>
2020-10-05 19:11:56 -0400

    Created Tower script + prefab, Leechbolt prefab

---
+++ Assets/Scripts/Controllers/Enemy/DestructibleTowerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleTowerController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject projectilePrefab;
+
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Transform lookAtPivot;
+
+    private void Start()
+    {
+        PoolManager.instance.CreatePool(projectilePrefab, 10);
+        StartCoroutine(IntermittentProjectileFire());
+    }
+    
+
+    private IEnumerator IntermittentProjectileFire()
+    {
+        while (target != null)
+        {
+            AimFire();
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    private void AimFire()
+    {
+        Vector3 adjustedTargetPosition = new Vector3(target.position.x, target.position.y + 0.25f, 0f);
+        Vector3 diff = adjustedTargetPosition - lookAtPivot.position;
+        diff.Normalize();
+        float zRotation = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        lookAtPivot.rotation = Quaternion.Euler(0f, 0f, zRotation);
+        PoolManager.instance.ReuseObject(projectilePrefab, lookAtPivot.position, lookAtPivot.rotation, this.tag);
+    }
+
+}
