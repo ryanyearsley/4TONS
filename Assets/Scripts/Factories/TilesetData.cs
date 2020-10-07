72388531cf9251a3644198cc0742fd57bcef6eb7
Ben Cutler <b.cutler724@gmail.com>
2020-10-07 00:04:22 -0400

    Add an indexer to tileset data and make it compatible with foreach loops with an IEnumerator.

--- Assets/Scripts/Factories/TilesetData.cs
+++ Assets/Scripts/Factories/TilesetData.cs
@@ -3,9 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu ( menuName = "Scriptable Object/Tileset")]
-public class TilesetData : ScriptableObject {
+public class TilesetData : ScriptableObject, IEnumerable<GameObject> {
 
 	[SerializeField]
 	public List<GameObject> tilePrefabs;
 
+	public GameObject this [int index] {
+		get {
+			return tilePrefabs [index];
+		}
+	}
+
+	public int Count {
+		get {
+			return tilePrefabs.Count;
+		}
+	}
+
+	public IEnumerator<GameObject> GetEnumerator () {
+		return tilePrefabs.GetEnumerator ();
+	}
+
+	IEnumerator IEnumerable.GetEnumerator () {
+		return GetEnumerator ();
+	}
 }
