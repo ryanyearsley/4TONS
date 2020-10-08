7dcfc1192e02334597c873dddaa5590d970f05cb
Ben Cutler <b.cutler724@gmail.com>
2020-10-08 18:45:41 -0400

    Updated the TilesetData and editor so there is a second list of set pieces for use in the random level generation.

--- Assets/Scripts/Factories/Editor/TilesetDataEditor.cs
+++ Assets/Scripts/Factories/Editor/TilesetDataEditor.cs
@@ -8,31 +8,57 @@ using UnityEngine;
 public class TilesetDataEditor : Editor {
 
 	private SerializedProperty tilePrefabs;
-	private ReorderableList list;
+	private SerializedProperty setPiecePrefabs;
+	private ReorderableList tileList;
+	private ReorderableList setPieceList;
 	private TilesetData tilesetData;
 
+	private bool tileListFoldout;
+	private bool setPieceListFoldout;
+
 	private void OnEnable () {
-		tilePrefabs = serializedObject.FindProperty ("tilePrefabs");
 		tilesetData = (TilesetData)target;
-		list = new ReorderableList (serializedObject, tilePrefabs, true, true, true, true);
-		list.drawHeaderCallback = DrawHeaderCallback;
-		list.drawElementCallback = DrawElementCallback;
-		list.elementHeightCallback += ElementHeightCallback;
-		list.onAddCallback += OnAddCallback;
+		tilePrefabs = serializedObject.FindProperty ("tilePrefabs");
+		tileList = new ReorderableList (serializedObject, tilePrefabs, true, true, true, true);
+		tileList.drawHeaderCallback = DrawTileHeaderCallback;
+		tileList.drawElementCallback = DrawTileElementCallback;
+		tileList.elementHeightCallback += TileElementHeightCallback;
+		tileList.onAddCallback += OnAddCallback;
+
+		setPiecePrefabs = serializedObject.FindProperty ("setPiecePrefabs");
+		setPieceList = new ReorderableList (serializedObject, setPiecePrefabs, true, true, true, true);
+		setPieceList.drawHeaderCallback = DrawSetPieceHeaderCallback;
+		setPieceList.drawElementCallback = DrawSetPieceElementCallback;
+		setPieceList.elementHeightCallback += SetPieceElementHeightCallback;
+		setPieceList.onAddCallback += OnAddCallback;
 	}
 
 	public override void OnInspectorGUI () {
 		serializedObject.Update ();
-		list.DoLayoutList ();
+
+		tileListFoldout = EditorGUILayout.BeginFoldoutHeaderGroup (tileListFoldout, "Tiles");
+		if (tileListFoldout)
+			tileList.DoLayoutList ();
+		EditorGUILayout.EndFoldoutHeaderGroup ();
+
+		setPieceListFoldout = EditorGUILayout.BeginFoldoutHeaderGroup (setPieceListFoldout, "Set Pieces");
+		if (setPieceListFoldout)
+			setPieceList.DoLayoutList ();
+		EditorGUILayout.EndFoldoutHeaderGroup ();
+
 		serializedObject.ApplyModifiedProperties ();
 	}
 
-	public void DrawHeaderCallback (Rect rect) {
+	public void DrawTileHeaderCallback (Rect rect) {
 		EditorGUI.LabelField (rect, "Tiles");
 	}
 
-	public void DrawElementCallback (Rect rect, int index, bool isActive, bool isFocusted) {
-		SerializedProperty element = list.serializedProperty.GetArrayElementAtIndex (index);
+	public void DrawSetPieceHeaderCallback (Rect rect) {
+		EditorGUI.LabelField (rect, "Set Pieces");
+	}
+
+	public void DrawTileElementCallback (Rect rect, int index, bool isActive, bool isFocusted) {
+		SerializedProperty element = tileList.serializedProperty.GetArrayElementAtIndex (index);
 		rect.y += 2;
 
 		GameObject prefab = tilesetData.tilePrefabs[index];
@@ -65,8 +91,48 @@ public class TilesetDataEditor : Editor {
 		}
 	}
 
-	public float ElementHeightCallback (int index) {
-		float propertyHeight = EditorGUI.GetPropertyHeight (list.serializedProperty.GetArrayElementAtIndex (index), false);
+	public void DrawSetPieceElementCallback (Rect rect, int index, bool isActive, bool isFocusted) {
+		SerializedProperty element = setPieceList.serializedProperty.GetArrayElementAtIndex (index);
+		rect.y += 2;
+
+		GameObject prefab = tilesetData.setPiecePrefabs[index];
+		string elementTitle = (prefab == null) ? "New Entry" : prefab.name;
+		EditorGUI.PropertyField (
+			new Rect (rect.x += 10, rect.y, Screen.width * 0.8f, height: EditorGUIUtility.singleLineHeight),
+			element, new GUIContent (elementTitle));
+
+		EditorGUI.LabelField (
+			new Rect (rect.x += 10,
+			rect.y += EditorGUIUtility.singleLineHeight,
+			Screen.width * 0.8f,
+			EditorGUIUtility.singleLineHeight),
+			"CSV Index: " + (tilesetData.tilePrefabs.Count + index + 1));
+
+		if (prefab != null) {
+			TileObject tileObject = prefab.GetComponent<TileObject> ();
+			if (tileObject != null) {
+				Texture2D texture = tileObject.spriteRenderer.sprite.texture;
+				EditorGUI.DrawPreviewTexture (
+					new Rect (
+						rect.x += Screen.width - 200,
+						rect.y += EditorGUIUtility.singleLineHeight,
+						100,
+						100),
+					texture);
+			} else {
+				Debug.Log ("The Set Piece (" + prefab.name + ") is missing the TileObject.cs component\n" + prefab);
+			}
+		}
+	}
+
+	public float TileElementHeightCallback (int index) {
+		float propertyHeight = EditorGUI.GetPropertyHeight (tileList.serializedProperty.GetArrayElementAtIndex (index), false);
+		float spacing = EditorGUIUtility.singleLineHeight / 2;
+		return propertyHeight * spacing;
+	}
+
+	public float SetPieceElementHeightCallback (int index) {
+		float propertyHeight = EditorGUI.GetPropertyHeight (setPieceList.serializedProperty.GetArrayElementAtIndex (index), false);
 		float spacing = EditorGUIUtility.singleLineHeight / 2;
 		return propertyHeight * spacing;
 	}
