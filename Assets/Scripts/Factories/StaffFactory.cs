1f6b991e7fa853ecc257195021dbd4554c6eb6a3
skiddmcmarx <41240083+skiddmcmarx@users.noreply.github.com>
2020-10-17 17:31:58 -0400

    Added Staff Factory (heh) and WizardBuildManager classes

---
+++ Assets/Scripts/Factories/StaffFactory.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Text.RegularExpressions;
+
+public class StaffFactory
+{
+    static Vector2 worldSpaceTileScale = new Vector2 (0.5f, 0.5f);
+
+    static string LINE_SPLIT_RE = @"\r\n|\n\r|\n|\r";
+	public static int [,] DeserializeStaffFile (TextAsset csv) {
+
+        string[] rows = Regex.Split(csv.text, LINE_SPLIT_RE).Where(s => !string.IsNullOrEmpty(s)).ToArray();
+        int rowCount = rows.Length;
+        Debug.Log ("rowCount: " + rowCount);
+        int columnCount = rows[0].Split(","[0]).Length;
+        Debug.Log ("columnCount: " + columnCount);
+        int[,] output = new int[columnCount, rowCount];
+        for (int y = 0; y < rowCount; y++) {
+            string currentRow = rows[rows.Length - (1 + y)];
+            string[] rowTiles = currentRow.Split(',');
+            for (int x = 0; x < columnCount; x++) {
+                output [x, y] = int.Parse (rowTiles [x]);
+            }
+        }
+        Debug.Log ("Staff file successfully processed.");
+        return output;
+    }
+    public static void BuildStaff (int [,] staffData, GameObject tile, Vector2 origin) {
+        int yLength = staffData.GetLength(1);
+        int xLength = staffData.GetLength(0);
+        Transform parent = new GameObject ("StaffParent").transform;
+        for (int y = 0; y < staffData.GetLength (1); y++) {
+            for (int x = 0; x < staffData.GetLength (0); x++) {
+                int tilePrefabIndex = staffData[x, y];
+                if (tilePrefabIndex != 0) {
+                    GameObject.Instantiate (
+                        tile, origin + (new Vector2(x, y) * worldSpaceTileScale),
+                        Quaternion.identity,
+                        parent);
+                }
+
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/Managers/WizardBuildManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WizardBuildManager : MonoBehaviour
+{
+	public TextAsset staffFile;
+	public GameObject tilePrefab;
+	public Transform staffOrigin;//bottom left corner
+
+
+	void Awake () {
+		int [,] staffTiles = StaffFactory.DeserializeStaffFile (staffFile);
+		StaffFactory.BuildStaff (staffTiles, tilePrefab, staffOrigin.position);
+	}
+}
