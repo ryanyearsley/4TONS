612db810b4f55d11c0ce1ad376043637c7907c48
skiddmcmarx <41240083+skiddmcmarx@users.noreply.github.com>
2020-10-30 02:44:39 -0400

    Reorganized CustomLevelGenerator scripts into package.

---
+++ Assets/Scripts/CustomLevelGenerator/UI/TilePaletteButtonUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TileUI : MonoBehaviour {
+
+	public Image tileImage;
+
+	private int tileIndex;
+
+	public void Initialize (int index, Sprite sprite, Color color) {
+		tileImage.sprite = sprite;
+		tileImage.color = color;
+		tileIndex = index;
+	}
+
+	public void OnButtonDown () {
+		LevelBuilderManager.instance.OnSelectTile (tileIndex);
+	}
+}
