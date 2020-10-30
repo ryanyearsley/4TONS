480575956c21f3133468b8925b6f9e68a3fe83d0
skiddmcmarx <41240083+skiddmcmarx@users.noreply.github.com>
2020-10-30 02:30:48 -0400

    Added wizard loadout data model

---
+++ Assets/Scripts/Wizard/Factory/WizardFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Input: WizardSaveData
+//Instantiates wizard prefab and injects save data into game object.
+public class WizardFactory : MonoBehaviour
+{
+}
---
+++ Assets/Scripts/Wizard/Model/WizardSaveData.cs
@@ -0,0 +1,9 @@
+﻿
+[System.Serializable]
+public class WizardSaveData {
+	public string wizardName;
+	public string spellSchoolDataPath;
+	public StaffSaveData primaryStaffSaveData;
+	public StaffSaveData secondaryStaffSaveData;
+	public SpellGemSaveData[] inventorySaveData;
+}
