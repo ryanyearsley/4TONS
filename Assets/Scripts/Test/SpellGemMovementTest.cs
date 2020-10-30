8659b871436ab1a43bc1ef822686421e7991da71
skiddmcmarx <41240083+skiddmcmarx@users.noreply.github.com>
2020-10-30 02:52:57 -0400

    spellgem movement test script

---
+++ Assets/Scripts/Test/SpellGemMovementTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpellGemMovementTest : MonoBehaviour
+{
+
+    public PuzzleUI spellgemUI;
+    public GameObject spellGem;
+
+	private void Start () {
+		spellgemUI.FitSpellgem (spellGem);
+	}
+}
