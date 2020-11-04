add8c01c74ae5660348bccf52a0b890b83e7066e
skiddmcmarx <41240083+skiddmcmarx@users.noreply.github.com>
2020-11-04 18:47:04 -0500

    Rephrasing in SpellSaveData model

--- Assets/Scripts/Wizard/Model/SpellSaveData.cs
+++ Assets/Scripts/Wizard/Model/SpellSaveData.cs
@@ -2,9 +2,8 @@
 using UnityEngine;
 [Serializable]
 public class SpellSaveData {
-
     public SpellData spellData;
     public string spellDataPath;
-    public Vector2Int spellGemCoordinates;
+    public Vector2Int spellGemOriginCoordinate;
     public int spellGemRotation;
 }
