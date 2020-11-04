9c808298280d98ebdaf1a902263ce65b80f4c1b3
skiddmcmarx <41240083+skiddmcmarx@users.noreply.github.com>
2020-11-04 02:49:49 -0500

    Added PrebuildWizardData + Default implementation

---
+++ Assets/Scripts/Wizard/SO/PrebuildWizardData.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu (fileName = "Data", menuName = "ScriptableObjects/Editor/PrebuildWizardData", order = 1)]
+
+public class PrebuildWizardData : ScriptableObject {
+	public WizardSaveData defaultSaveData;
+}
+
