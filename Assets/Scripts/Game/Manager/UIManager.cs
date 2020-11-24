60c1121317c07f976f5f5d583f88c1220ebbce60
skiddmcmarx <41240083+skiddmcmarx@users.noreply.github.com>
2020-11-24 17:43:54 -0500

    added quick/dirty controls UI

--- Assets/Scripts/Game/Manager/UIManager.cs
+++ Assets/Scripts/Game/Manager/UIManager.cs
@@ -1,11 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
 	#region Singleton
 	public static UIManager Instance { get; private set; }
+
+	bool displayingControls;
+	public Text controlsText;
 	private void InitializeSingleton () {
 		Instance = this;
 	}
@@ -14,5 +18,17 @@ public class UIManager : MonoBehaviour
 
 	private void Awake () {
 		InitializeSingleton ();
+	}
+
+	private void Update () {
+		if (Input.GetKeyDown(KeyCode.C)) {
+			if (!displayingControls) {
+				displayingControls = true;
+				controlsText.gameObject.SetActive (true);
+			} else{
+				displayingControls = false;
+				controlsText.gameObject.SetActive (false);
+			}
+		}
 	}
 }
