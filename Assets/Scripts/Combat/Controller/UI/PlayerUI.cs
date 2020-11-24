483e5ae79ace4beced4fa759cadf784ef5cf225d
skiddmcmarx <41240083+skiddmcmarx@users.noreply.github.com>
2020-11-24 17:41:26 -0500

    removed player stocks from player UI

--- Assets/Scripts/Combat/Controller/UI/PlayerUI.cs
+++ Assets/Scripts/Combat/Controller/UI/PlayerUI.cs
@@ -5,12 +5,11 @@ public class PlayerUI : MonoBehaviour
 {
 	public Text playerName;
 	public Image playerPortait;
-	public Image playerStocks;
 	public SpellUI[] spellUIs;
 
 	public void InitializePlayerUI (Player player) {
 		playerName.text = player.currentWizard.wizardName;
+		playerName.color = player.currentWizard.spellSchoolData.schoolGemColor;
 		playerPortait.sprite = player.currentWizard.spellSchoolData.portrait;
-		playerStocks.fillAmount = 1;
 	}
 }
