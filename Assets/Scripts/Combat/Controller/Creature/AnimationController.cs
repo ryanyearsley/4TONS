295cc415497c56da76a773d7bae1db2dc3886454
skiddmcmarx <41240083+skiddmcmarx@users.noreply.github.com>
2020-11-25 00:23:59 -0500

    Abstracted animation controller and made inheriting scripts

--- Assets/Scripts/Combat/Controller/Creature/AnimationController.cs
+++ Assets/Scripts/Combat/Controller/Creature/AnimationController.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 [RequireComponent (typeof (SpriteRenderer), typeof (Animator))]
 public class AnimationController : MonoBehaviour {
 
-    private PlayerStateController stateController;
+    protected AbstractStateController stateController;
 
-    private SpriteRenderer playerSprite;
-    private Animator animator;
+    protected SpriteRenderer sprite;
+    protected Animator animator;
 
     private void Awake () {
-        playerSprite = GetComponent<SpriteRenderer> ();
+        sprite = GetComponent<SpriteRenderer> ();
         animator = GetComponent<Animator> ();
     }
     public void InitializeComponent (Player player) {
@@ -27,22 +27,8 @@ public class AnimationController : MonoBehaviour {
         animator.Rebind ();
     }
 
-    public void OnDash (DashInfo dashInfo) {
-        StartCoroutine (DashRoutine (dashInfo));
-    }
-
-    private IEnumerator DashRoutine (DashInfo dashInfo) {
-        if (!stateController.isDead)
-            animator.SetTrigger ("rollDodge");
-        playerSprite.color = new Color (1, 1, 1, 0.5f);
-        yield return new WaitForSeconds (dashInfo.invulnerableTime);
-        playerSprite.color = new Color (1, 1, 1, 1);
-
-
-    }
-
     public void OnSetFaceDirection (int faceDirection) {
-        playerSprite.flipX = (faceDirection < 0);
+        sprite.flipX = (faceDirection < 0);
     }
 
     public void OnSetVelocity (Vector2 velocity) {
@@ -55,34 +41,26 @@ public class AnimationController : MonoBehaviour {
     public void OnHit (OnHitInfo onHitInfo) {
         if (!stateController.isDead)
         {
-            playerSprite.flipX = (Mathf.Sign(onHitInfo.direction.x) > 0);
+            sprite.flipX = (Mathf.Sign(onHitInfo.direction.x) > 0);
             animator.SetTrigger("hit");
         }
     }
 
-    public void OnCastSpell(SpellData spellData) {
-        if (!stateController.isDead) {
-        }
-    }
 
-    private void OnEnable () {
-        stateController = transform.parent.GetComponent<PlayerStateController> ();
+    protected virtual void OnEnable () {
+        stateController = transform.parent.GetComponent<AbstractStateController> ();
         stateController.OnDeathEvent += OnDeath;
         stateController.OnRespawnEvent += OnRespawn;
-        stateController.OnDashEvent += OnDash;
         stateController.OnSetFaceDirEvent += OnSetFaceDirection;
         stateController.OnSetVelocityEvent += OnSetVelocity;
         stateController.OnHitEvent += OnHit;
-        stateController.OnCastSpellEvent += OnCastSpell;
     }
 
     private void OnDisable () {
         stateController.OnDeathEvent -= OnDeath;
         stateController.OnRespawnEvent -= OnRespawn;
-        stateController.OnDashEvent -= OnDash;
         stateController.OnSetFaceDirEvent -= OnSetFaceDirection;
         stateController.OnSetVelocityEvent -= OnSetVelocity;
         stateController.OnHitEvent -= OnHit;
-        stateController.OnCastSpellEvent -= OnCastSpell;
     }
 }
---
+++ Assets/Scripts/Combat/Controller/Creature/Player/PlayerAnimationController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationController : AnimationController {
+
+    PlayerStateController playerStateController;
+    public void OnDash (DashInfo dashInfo) {
+        StartCoroutine (DashRoutine (dashInfo));
+    }
+
+
+    public void OnCastSpell (SpellData spellData) {
+        if (!stateController.isDead) {
+            //cast animation trigger
+        }
+    }
+
+    private IEnumerator DashRoutine (DashInfo dashInfo) {
+        if (!stateController.isDead)
+            animator.SetTrigger ("rollDodge");
+        sprite.color = new Color (1, 1, 1, 0.5f);
+        yield return new WaitForSeconds (dashInfo.invulnerableTime);
+        sprite.color = new Color (1, 1, 1, 1);
+    }
+
+	protected override void OnEnable () {
+		base.OnEnable ();
+        playerStateController = GetComponentInParent<PlayerStateController> ();
+        playerStateController.OnDashEvent += OnDash;
+        playerStateController.OnCastSpellEvent += OnCastSpell;
+    }
+}
