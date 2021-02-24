90ff72f4637d217db639695f2fd31b7dd175c704
skiddmcmarx <41240083+skiddmcmarx@users.noreply.github.com>
2021-02-24 18:26:22 -0500

    wizard model update

--- Assets/Scripts/Wizard/Model/WizardSaveData.cs
+++ Assets/Scripts/Wizard/Model/WizardSaveData.cs
@@ -2,11 +2,25 @@
 using System.Collections.Generic;
 
 [Serializable]
-public class WizardSaveData {
+public class WizardSaveData : ICloneable {
 	public string wizardName;
 	public SpellSchoolData spellSchoolData;
 	public string spellSchoolDataPath;
-	public StaffSaveData primaryStaffSaveData;
-	public StaffSaveData secondaryStaffSaveData;
-	public PuzzleSaveDataDictionary inventorySaveDataDictionary;
+	public PuzzleSaveData primaryStaffSaveData;
+	public PuzzleSaveData secondaryStaffSaveData;
+	public PuzzleSaveData inventorySaveData;
+
+	public WizardSaveData Clone () {
+		WizardSaveData wizard =  (WizardSaveData) this.MemberwiseClone();
+		wizard.primaryStaffSaveData = new PuzzleSaveData (primaryStaffSaveData.puzzleData, primaryStaffSaveData.puzzleSaveDataDictionary);
+		wizard.secondaryStaffSaveData = new PuzzleSaveData (secondaryStaffSaveData.puzzleData, secondaryStaffSaveData.puzzleSaveDataDictionary);
+		wizard.inventorySaveData = new PuzzleSaveData (inventorySaveData.puzzleData, inventorySaveData.puzzleSaveDataDictionary);
+		return wizard;
+	}
+
+	object ICloneable.Clone () {
+		return Clone ();
+	}
+
+
 }
