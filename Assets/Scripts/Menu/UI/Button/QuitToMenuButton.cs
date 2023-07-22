853b0d6ed0386c3cf36da3a9bf9c65614fa07696
ryanyearsley <ryan.a.yearsley@gmail.com>
2023-07-22 16:33:34 -0400

    Add ConfirmDeath/ClearWizards to GameOver buttons

--- Assets/Scripts/Menu/UI/Button/QuitToMenuButton.cs
+++ Assets/Scripts/Menu/UI/Button/QuitToMenuButton.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using NERDSTORM;
 public class QuitToMenuButton : AbstractButtonClick {
+	GauntletGameOverPanelUI gameOverPanelUI;
 
-	public override void OnClick () {
-		Player playerOne = PlayerManager.instance.currentPlayers[0];
-		//SaveManager.instance.SaveNewWizardDataJSON (playerOne.wizardSaveData);
-		playerOne.wizardSaveData = null;
-		playerOne.currentPlayerObject = null;
-		playerOne.isReady = false;
-		playerOne.isAlive = false;
-		NerdstormSceneManager.instance.LoadMenu ();
+	protected override void Awake()
+	{
+		base.Awake();
+		gameOverPanelUI = GetComponentInParent<GauntletGameOverPanelUI>();
+	}
+
+	public override void OnClick()
+	{
+		gameOverPanelUI.ConfirmDeathInfo();
+		PlayerManager.instance.ClearSelectedWizards();
+		NerdstormSceneManager.instance.LoadMenu();
 	}
 }
--- Assets/Scripts/Menu/UI/Button/RestartGauntletButtonClick.cs
+++ Assets/Scripts/Menu/UI/Button/RestartGauntletButtonClick.cs
@@ -5,10 +5,19 @@ using NERDSTORM;
 
 public class RestartGauntletButtonClick : AbstractButtonClick {
 
+	GauntletGameOverPanelUI gameOverPanelUI;
+
+	protected override void Awake()
+	{
+		base.Awake();
+		gameOverPanelUI = GetComponentInParent<GauntletGameOverPanelUI>();
+	}
+
 	public override void OnClick () {
+		gameOverPanelUI.ConfirmDeathInfo();
+		PlayerManager.instance.ClearSelectedWizards(); 
 		Player playerOne = PlayerManager.instance.currentPlayers[0];
-		playerOne.wizardSaveData = playerOne.wizardSaveData.wizardData.gauntletStartData.wizardSaveData.Clone ();
-		playerOne.currentPlayerObject = null;
+		playerOne.wizardSaveData = playerOne.wizardSaveData.wizardData.gauntletStartData.wizardSaveData.Clone();
 		NerdstormSceneManager.instance.LoadGauntletTowerScene (Zone.Hub);
 	}
 }
